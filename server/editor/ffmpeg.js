c728d38a2fc588118c0231b308514f0cbeb9757a
crosscripter <crosscripter@gmail.com>
2022-08-23 14:34:56 -0500

    Added audio / video stream merging  refactored ffmpeg commands

--- server/editor/ffmpeg.js
+++ server/editor/ffmpeg.js
@@ -4,6 +4,8 @@ const ffmpeg = require('fluent-ffmpeg')
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
 ffmpeg.setFfmpegPath(ffmpegPath)
 
+const base = `./server/public`
+
 // ffmpeg -i input1.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts intermediate1.ts
 const createIntermediate = async (file) => {
   log('ffmpeg: transcoding', file, 'to MPEG-2 transport stream (H.264/AAC)...')
@@ -23,24 +25,38 @@ const createIntermediate = async (file) => {
   })
 }
 
-const concatMedia = (ext, options) => async (files) => {
-  const base = `./server/public`
+const resolveFiles = async (files) => {
   const names = await Promise.all(
     files.map((f) => createIntermediate(`${base}/${f}`))
   )
 
-  const namesString = names.join('|')
-  log('ffmpeg: concatenating media ', namesString, '...')
+  log('ffmpeg: resolved media paths', names)
+  return names
+}
 
-  await new Promise((resolve, reject) =>
-    ffmpeg(`concat:${namesString}`)
+const ffmpegCli = async (inputs, output, ...options) => {
+  return await new Promise((resolve, reject) => {
+    const input = Array.isArray(inputs)
+      ? inputs.reduce(
+          (result, inputItem) => result.addInput(inputItem),
+          ffmpeg()
+        )
+      : ffmpeg(inputs)
+
+    input
       .outputOptions(...options)
-      .output(`${base}/output.${ext}`)
+      .output(`${base}/${output}`)
       .on('end', resolve)
       .on('error', reject)
       .run()
-  )
+  })
+}
 
+const concatMedia = (ext, options) => async (files) => {
+  const names = await resolveFiles(files)
+  const namesString = names.join('|')
+  log('ffmpeg: concatenating media ', namesString, '...')
+  await ffmpegCli(`concat:${namesString}`, `output.${ext}`, ...options)
   names.forEach(unlinkSync)
   log(`ffmpeg: ${files.length} ${ext}s concatenated successfully`)
 }
@@ -51,4 +67,25 @@ const concatmp3 = concatMedia('mp3', ['-acodec', 'copy'])
 // ffmpeg -i "concat:intermediate1.ts|intermediate2.ts" -c copy -bsf:a aac_adtstoasc output.mp4
 const concatmp4 = concatMedia('mp4', ['-c', 'copy', '-bsf:a', 'aac_adtstoasc'])
 
-module.exports = { concatmp3, concatmp4 }
+// ffmpeg -i input.mp4 -i input.mp3 -c copy -map 0:v:0 -map 1:a:0 output.mp4
+const concatAV = async (files) => {
+  const [video, audio] = files
+  log(`ffmpeg: Adding audio ${audio} to video ${video}...`)
+  const names = await resolveFiles(files)
+  log('ffmpeg: editing media ', names, '...')
+  await ffmpegCli(
+    names,
+    `output.mp4`,
+    '-c',
+    'copy',
+    '-map',
+    '0:v:0',
+    '-map',
+    '1:a:0',
+    '-shortest'
+  )
+  names.forEach(unlinkSync)
+  log(`ffmpeg: Audio track ${audio} added to ${video} video successfully!`)
+}
+
+module.exports = { concatmp3, concatmp4, concatAV }
--- server/editor/index.js
+++ server/editor/index.js
@@ -9,9 +9,11 @@ const types = { mp4: 'video', mp3: 'audio' }
 router.get('/concat', async ({ query: { files } }, res) => {
   files = files.split(',')
   const ext = parse(files[0]).ext.slice(1)
-  console.log('ext', ext)
+  const ext2 = parse(files[1]).ext.slice(1)
+  console.log('ext', ext, 'ext2', ext2)
   const type = types[ext]
-  await ffmpeg[`concat${ext}`](files)
+  const isAV = ext === 'mp4' && ext !== ext2
+  await ffmpeg[`concat${isAV ? 'AV' : ext}`](files)
   files.push(`output.${ext}`)
 
   return res.send(
