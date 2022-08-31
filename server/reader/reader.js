d927e73c2cbb3fda62174d57f9eb67a9341790d8
Michael Schutt <crosscripter@gmail.com>
2022-08-31 15:54:06 -0500

    refactoring

--- server/reader/reader.js
+++ server/reader/reader.js
@@ -1,12 +1,9 @@
 const say = require('say')
 const { parse } = require('path')
 const { log } = require('../logger')
+const { tempName } = require('../utils')
 const { wav2mp3 } = require('../editor/ffmpeg')
 const { readFileSync, writeFileSync } = require('fs')
-
-const base = './server/public'
-const tempName = (ext) =>
-  `${base}/${Math.random().toString(13).slice(2)}.${ext}`
 
 const transcribe = (audio) => {
   const out = tempName('srt')
@@ -21,11 +18,7 @@ const _say = async (text) => {
 
   return new Promise((res, rej) => {
     say.export(text, 'Microsoft Zira Desktop', 1.0, output, async (err) => {
-      if (err) {
-        log('tts error', err)
-        return rej(err)
-      }
-
+      if (err) return rej(err)
       output = await wav2mp3(output)
       log(`tts: Text "${text}" exported to ${output}`)
       return res(parse(output).base)
