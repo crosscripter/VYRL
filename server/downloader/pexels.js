adb217cc480c9fc90a0cf2b9217973dbd880be10
Michael Schutt <crosscripter@gmail.com>
2022-08-31 15:42:02 -0500

    Refactor pexels, removed download

--- server/downloader/pexels.js
+++ server/downloader/pexels.js
@@ -1,78 +1,27 @@
 require('dotenv').config()
 const axios = require('axios')
 const { log } = require('../logger')
-const { tempName } = require('../utils')
+const { basename } = require('path')
 const { createClient } = require('pexels')
-const { basename, parse } = require('path')
-const { createWriteStream, existsSync } = require('fs')
+const { createWriteStream } = require('fs')
+const { fileExt, tempName } = require('../utils')
 
 const { PEXELS_API_KEY } = process.env
 const client = createClient(PEXELS_API_KEY)
 
-const download = async (url) => {
-  const fileName = basename(url).split('?')[0].trim()
-  const ext = parse(fileName).ext.slice(1)
-  const localFilePath = tempName(ext) // join('./server/public', fileName)
-
-  if (existsSync(localFilePath)) {
-    log('file', localFilePath, 'exists')
-    return localFilePath
-  }
-
-  return new Promise(async (res, rej) => {
-    try {
-      log('Downloading', url, 'to', localFilePath, '...')
-      const response = await axios({
-        url,
-        method: 'GET',
-        responseType: 'stream',
-      })
-      log('Writing file stream', localFilePath, '...')
-      const stream = response.data.pipe(createWriteStream(localFilePath))
-      stream.on('finish', () => {
-        log('File written', localFilePath)
-        res(localFilePath)
-      })
-    } catch (err) {
-      log('Download failed', err)
-      rej(err)
-    }
-  })
-}
-
 const search = async (query, per_page) => {
   log('Searching for', query, 'videos on Pexel...')
   const { videos } = await client.videos.search({ query, per_page })
+  const isVideoHD = ({ file_type, quality, width }) => file_type === 'video/mp4' && quality === 'hd'
+  const getTitleFromUrl = url => url.slice(0, -2).split('/').slice(-1)[0].split('-').slice(0, -1).join(' ')
 
-  return videos
-    .map((result) => {
-      const {
-        duration,
-        url,
-        width,
-        user: { name },
-        video_files,
-      } = result
-
-      if (duration < 10 || width < 1920) return null
-
-      const hdfiles = video_files.filter(
-        ({ file_type, quality, width }) =>
-          file_type === 'video/mp4' && quality === 'hd'
-      )
-
-      const title = url
-        .slice(0, -2)
-        .split('/')
-        .slice(-1)[0]
-        .split('-')
-        .slice(0, -1)
-        .join(' ')
-
-      const hqfile = hdfiles.slice(-1)[0]
-      return { url, title, duration, name, video: hqfile.link }
-    })
-    .filter(Boolean)
+  return videos.map((result) => {
+    const { duration, url, width, user: { name }, video_files } = result
+    if (duration < 10 || width < 1920) return null
+    const title = getTitleFromUrl(url)
+    const video = video_files.filter(isVideoHD).slice(-1)[0]?.link
+    return { url, title, duration, name, video }
+  }).filter(Boolean)
 }
 
-module.exports = { search, download }
+module.exports = { search }
