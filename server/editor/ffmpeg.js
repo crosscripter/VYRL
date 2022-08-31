c52e3af305715dbdd8a967f8b3d17f032a0d6151
Michael Schutt <crosscripter@gmail.com>
2022-08-31 15:15:40 -0500

    Added options and removed REFRAME filter

--- server/editor/ffmpeg.js
+++ server/editor/ffmpeg.js
@@ -7,8 +7,17 @@ const ffmpeg = require('fluent-ffmpeg')
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
 ffmpeg.setFfmpegPath(ffmpegPath)
 
+const options = {
+  CONCATMP3: '-acodec copy',
+  LOOP_INPUT: `-stream_loop -1`,
+  CONCATMP4: '-c copy -bsf:a aac_adtstoasc',
+  LOOP_OUTPUT: secs => `-c copy -t ${secs}`,
+  SUBTITLE: file => `-vf subtitles=${file}`,
+  REFRAME: scale => `-filter:v setpts=${scale}*PTS`,
+  TRANSCODE: '-c copy -bsf:v h264_mp4toannexb -f mpegts'
+}
+
 const filters = {
-  REFRAME: scale => `setpts=${scale}*PTS`,
   FADE: duration => `fade=t=in:st=0:d=2,fade=t=out:st=${+duration - 2}:d=2`,
   VOICEOVER: '[0:0]volume=0.3[a];[1:0]volume=2.0[b];[a][b]amix=inputs=2:duration=longest',
   DRAWTEXT: ([text, y, size = 42]) => `drawtext=font=verdana:text='${text}':fontcolor=white:fontsize=${size}:x=20:y=h-th-${y}`,
@@ -19,9 +28,12 @@ const filters = {
 
 const _ffmpeg = (inputs, ext, outputOptions, filter, inputOptions) => {
   return new Promise((resolve, reject) => {
+    inputOptions = inputOptions?.split(' ')
+    outputOptions = outputOptions?.split(' ')
+
     const out = tempName(ext)
     inputs = resolveFiles(Array.isArray(inputs) ? inputs : [inputs])
-    
+
     log('ffmpeg', 
         ...inputOptions, 
         ...inputs.map(i => `-i ${i}`), 
@@ -49,28 +61,30 @@ const _ffmpeg = (inputs, ext, outputOptions, filter, inputOptions) => {
   })
 }
 
+
+const concatmp3 = concatMedia('mp3', options.CONCATMP3)
+
+const concatmp4 = concatMedia('mp4', options.CONCATMP4)
+
 const wav2mp3 = async (wav) => await _ffmpeg(wav, 'mp3')
 
-const concatmp3 = concatMedia('mp3', ['-acodec', 'copy'])
+const transcode = async (file) => await _ffmpeg(file, 'ts', options.TRANSCODE)
 
-const concatmp4 = concatMedia('mp4', ['-c', 'copy', '-bsf:a', 'aac_adtstoasc'])
+const voiceOver = async (files) => await _ffmpeg(files, 'mp3', '-y', filters.VOICEOVER)
 
-const voiceOver = async (files) => await _ffmpeg(files, 'mp3', ['-y'], filters.VOICEOVER)
+const watermark = async (files) => await _ffmpeg(files, 'mp4', '-c:a copy', filters.WATERMARK)
 
-const watermark = async (files) => await _ffmpeg(files, 'mp4', ['-c:a', 'copy'], filters.WATERMARK)
+const reframe = async (video, scale=2.0) => await _ffmpeg(video, 'mp4', options.REFRAME(scale))
 
-const subtitle = async ([video, subtitle]) =>  await _ffmpeg(video, 'mp4', ['-vf', `subtitles=${subtitle}`])
+const subtitle = async ([video, subtitle]) =>  await _ffmpeg(video, 'mp4', options.SUBTITLE(subtitle))
 
-const reframe = async (video, scale=2.0) => await _ffmpeg(video, 'mp4', ['-filter:v', filters.REFRAME(scale)])
+const loop = async (file, secs) => await _ffmpeg(file, fileExt(file), options.LOOP_OUTPUT(secs), null, options.LOOP_INPUT)
 
-const loop = async (file, secs) => await _ffmpeg(file, fileExt(file), ['-c', 'copy', '-t', secs], null, ['-stream_loop', '-1'])
-
-const createIntermediate = async (file) => await _ffmpeg(file, 'ts', ['-c', 'copy', '-bsf:v', 'h264_mp4toannexb', '-f', 'mpegts'])
 
 const concatAV = async (files) => {
   const [video, audio] = files
   log(`ffmpeg: Adding audio ${audio} to video ${video}...`)  
-  const out = await _ffmpeg(files, 'mp4', ['-c', 'copy', '-map', '0:v', '-map', '1:a'])
+  const out = await _ffmpeg(files, 'mp4', '-c copy -map 0:v -map 1:a')
   log(`ffmpeg: Audio track ${audio} added to ${video} video as ${out} successfully`)
   return out
 }
@@ -85,7 +99,7 @@ const fade = async ({ file, duration }) => {
 
 const concatMedia = (ext, options) => async (files) => {
   const rfiles = resolveFiles(files)
-  const names = await Promise.all(rfiles.map(createIntermediate))
+  const names = await Promise.all(rfiles.map(transcode))
   const namesString = names.join('|')
   log('ffmpeg: concatenating media ', namesString, '...')
   const out = await _ffmpeg(`concat:${namesString}`, ext, options)
