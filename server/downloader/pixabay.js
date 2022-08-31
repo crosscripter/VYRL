bcb75493cfa167cbc0851f5e7e13b0f7f7135c6d
Michael Schutt <crosscripter@gmail.com>
2022-08-31 15:51:47 -0500

    Fixed renamed imports

--- server/downloader/pixabay.js
+++ server/downloader/pixabay.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 const { log } = require('../logger')
-const { scrape } = require('./scraper')
+const { scrapeTracks } = require('./scraper')
 const { PIXABAY_URL } = process.env
 
 const search = async (genre) => {
   log(`pixabay: searching for ${genre} music from ${PIXABAY_URL}...`)
-  return scrape(PIXABAY_URL, genre)
+  return scrapeTracks(PIXABAY_URL, genre)
 }
 
 module.exports = { search }
