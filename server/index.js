1ab35c0aa25e8a43fb592577d0f4d9f8e659c1a6
crosscripter <crosscripter@gmail.com>
2022-09-08 22:35:05 -0500

    Refactored server and added Nextron client! Cleaned up deps

--- server/index.js
+++ server/index.js
@@ -1,43 +1,22 @@
-require('dotenv').config()
+const spec = require('./spec')
 const express = require('express')
 const { log } = require('./logger')
 const { produce } = require('./producer')
-const { thumbnail } = require('./editor/ffmpeg')
+const { SERVER_HOST, SERVER_PORT } = require('./config')
+const SERVER_URL = `${SERVER_HOST}:${SERVER_PORT}`
 
 app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const { SERVER_HOST, SERVER_PORT } = process.env
-const SERVER_URL = `${SERVER_HOST}:${SERVER_PORT}`
-
 app.use(express.static('./server/public'))
 app.use('/edit', require('./editor'))
 app.use('/tts', require('./reader'))
-app.use('/video', require('./downloader/video'))
-app.use('/audio', require('./downloader/audio'))
 
 app.get('/', (_, res) => res.send(`<h1><tt>VYRL Server</tt></h1>`))
 
 app.listen(3000, async () => {
   console.clear()
   log(`Server launched at ${SERVER_URL} 🚀`)
-
-  return await thumbnail('assets/mtn.mp4', 'The BEST video ever!')
-
-  await produce({
-    duration: 30,
-    captions: true,
-    audio: {
-      fade: true,
-      theme: 'chill',
-    },
-    video: {
-      fade: true,
-      scale: true,
-      watermark: true,
-      theme: 'space planet galaxy stars',
-      hashtags: '#music, #relax, #space, #stars',
-    },
-  })
+  await produce(spec)
 })
